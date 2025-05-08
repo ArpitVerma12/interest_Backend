41e95f21404b3e17a575865e31aa53a4c1337804
Arpit Verma <107420889+ArpitVerma12@users.noreply.github.com>
2025-05-08 11:50:53 +0530

    cors fixed

--- src/main/java/com/backend/ForwardedHeaderConfig.java
+++ src/main/java/com/backend/ForwardedHeaderConfig.java
@@ -1,18 +1,18 @@
-//package com.backend;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.boot.web.servlet.FilterRegistrationBean;
-//import org.springframework.web.filter.ForwardedHeaderFilter;
-//
-//@Configuration
-//public class ForwardedHeaderConfig {
-//
-//    @Bean
-//    public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
-//        FilterRegistrationBean<ForwardedHeaderFilter> filterRegBean = new FilterRegistrationBean<>();
-//        filterRegBean.setFilter(new ForwardedHeaderFilter());
-//        return filterRegBean;
-//    }
-//}
-//
+package com.backend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.web.filter.ForwardedHeaderFilter;
+
+@Configuration
+public class ForwardedHeaderConfig {
+
+    @Bean
+    public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+        FilterRegistrationBean<ForwardedHeaderFilter> filterRegBean = new FilterRegistrationBean<>();
+        filterRegBean.setFilter(new ForwardedHeaderFilter());
+        return filterRegBean;
+    }
+}
+
