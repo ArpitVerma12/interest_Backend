8044e075d0b0fd0c46e546caf2219aa8d9056aeb
Arpit Verma <107420889+ArpitVerma12@users.noreply.github.com>
2025-09-25 12:21:12 +0530

    fix

--- src/main/java/com/backend/controller/overDueController.java
+++ src/main/java/com/backend/controller/overDueController.java
@@ -5,7 +5,10 @@ import java.time.LocalDateTime;
 import java.time.Period;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.Objects;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -78,7 +81,8 @@ public class overDueController {
 			            }
 			            
 			            	
-			            
+			            if(item.getGiveMoney()!=null && item.getInterest()!=null) {
+			            	
 			            double rentMoney=((Double.parseDouble(item.getGiveMoney()) * item.getInterest())/100)*monthForRent;
 			            double totalMoney = Double.parseDouble(item.getGiveMoney()) + rentMoney;
 			          
@@ -95,8 +99,15 @@ public class overDueController {
 			            dto.setRemark(item.getRemark());
 			            dto.setVillage(item.getNewCustomer().getVillage());
 			            return dto;
+			            
+			            }
+			            return null;
 			        })
+			        
 			        .toList();
+		 if (dtoList.isEmpty()) {
+		        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Data not found");
+		    }
 		return ResponseEntity.ok().body(dtoList);
 	}
 	
